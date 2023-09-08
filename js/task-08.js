a651c4bed57e691886ba47d85c25bf8963b9b590
Oksana Zinchenko <zkosanaa@gmail.com>
2023-09-09 00:53:41 +0300

    task-7-8-9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const loginForm = document.querySelector('.login-form');
 
+  loginForm.addEventListener('submit', function (e) {
+    e.preventDefault(); // Забороняємо стандартну поведінку форми (перезавантаження сторінки)
+
+    const formData = new FormData(loginForm);
+
+    const formDataObject = {};
+    formData.forEach((value, key) => {
+      formDataObject[key] = value;
+    });
+
+    if (!formDataObject.email || !formDataObject.password) {
+      alert('Усі поля повинні бути заповнені');
+      return;
+    }
+
+    console.log(formDataObject);
+
+    loginForm.reset(); // Очищуємо значення полів форми
+  });
