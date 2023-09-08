9022bc1d6390e78dc26b75f2b9f9896c3a9d7a57
Oksana Zinchenko <zkosanaa@gmail.com>
2023-09-09 00:05:26 +0300

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,25 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ul = document.getElementById("ingredients");
+
+const fragment = document.createDocumentFragment();
+
+ingredients.forEach((ingredient) => {
+  const li = document.createElement("li");
+  li.textContent = ingredient;
+  li.classList.add("item");
+  fragment.appendChild(li);
+});
+
+ul.appendChild(fragment);
+
+
+
+
+
+
+
+
+
+
