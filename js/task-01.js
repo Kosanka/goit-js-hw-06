a7165ca3e09cfdd166b9a86facb9aa3a3387fb0c
Oksana Zinchenko <zkosanaa@gmail.com>
2023-09-08 23:53:56 +0300

    +

--- js/task-01.js
+++ js/task-01.js
@@ -1,12 +1,12 @@
 const categoriesList = document.querySelector('#categories');
 const categoriesItems = categoriesList.querySelectorAll('.item');
 
-console.log(`Total categories: ${categoriesItems.length}`);
+console.dir(`Total categories: ${categoriesItems.length}`);
 
 categoriesItems.forEach(categoryItem => {
   const categoryTitle = categoryItem.querySelector('h2').textContent;
   const categoryItemsCount = categoryItem.querySelectorAll('li').length;
   
-  console.log(`Category: ${categoryTitle}`);
-  console.log(`Elements count: ${categoryItemsCount}`);
+  console.dir(`Category: ${categoryTitle}`);
+  console.dir(`Elements count: ${categoryItemsCount}`);
 });
